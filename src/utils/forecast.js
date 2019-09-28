a9f08bfe7a8ef5fb2d1c2504365474b2c29eba7d
Dstonefire <dstonefire@gmail.com>
2019-09-28 00:13:41 -0600

    Add temperature High and Low

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,9 @@
 const request = require('request');
 
+// Littleton
+// longitude: -105.0166
+// latitude: 39.6133
+
 const forecast = (latitude, longitude, callback) => {
 
     const url = 'https://api.darksky.net/forecast/8675ddd98799f6a683f191d2d1cf3be5/' + latitude  + ',' + longitude;
@@ -13,8 +17,13 @@ const forecast = (latitude, longitude, callback) => {
             const currentTemp = body.currently.temperature;
             const precipProb = body.currently.precipProbability;
             const summary = body.daily.data[0].summary;
+            const tempHigh = body.daily.data[0].temperatureHigh;
+            const tempLow = body.daily.data[0].temperatureLow;
+            const message1 = summary + ' It is currently ' + currentTemp + " degrees. ";
+            const message2 = "There is a " + precipProb + "% chance of precipitation. ";
+            const message3 = "The high temperature today was " + tempHigh + " and low temperature today was " + tempLow;
 
-            callback(undefined, summary + ' It is currently ' + currentTemp + " degrees with " + precipProb + "% chance of precipitation.")
+            callback(undefined, message1 + message2 + message3);
         }
     })
 }
